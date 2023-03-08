79267f67b918cabe33292eae036730237f9aa1a0
Giordano Temple <giordanotemple@gmail.com>
2023-03-08 20:25:22 +0000

    add basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
