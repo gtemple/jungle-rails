d9c7b658e1081bed6969354e3094f5671f2df9ee
Giordano Temple <giordanotemple@gmail.com>
2023-03-09 22:03:35 +0000

    add bcrypt routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
