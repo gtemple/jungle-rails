5bada24b931cfca48b3205108ebf9cb77fca8858
Giordano Temple <giordanotemple@gmail.com>
2023-03-09 22:17:29 +0000

    add final login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   
