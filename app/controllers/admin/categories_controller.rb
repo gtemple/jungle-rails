96ec205f1e9bee43ae55d0995d80b601615cdb2f
Giordano Temple <giordanotemple@gmail.com>
2023-03-09 17:42:02 +0000

    add all controller logic

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,19 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.order(id: :desc).all
 
   end
-  
-  def new
 
+  def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
 
-  end
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
 end
