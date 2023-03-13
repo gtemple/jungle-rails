b1f199243d6d561a004deb70ab87764d64c90c4b
Giordano Temple <giordanotemple@gmail.com>
2023-03-13 17:11:42 +0000

    not valid without name test pass

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Product, type: :model do
 
   describe 'Validations' do
-    it 'must have a name' do
+    it 'Is valid with all fields' do
       @category = Category.new
       @product = Product.new({
         name:  'Cliff Collard',
@@ -12,7 +12,19 @@ RSpec.describe Product, type: :model do
         category: @category,
         price: 79.99
       })
-      expect (@product)to be_valid
+      expect(@product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @category = Category.new
+      @product = Product.new({
+        name:  nil,
+        description: "hello",
+        quantity: 23,
+        category: @category,
+        price: 79.99
+      })
+      expect(@product).to_not be_valid
     end
   end
 end
