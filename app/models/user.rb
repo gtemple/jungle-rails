f9e2dfc35ad7ba87871b037f2e4812fad69d9226
Giordano Temple <giordanotemple@gmail.com>
2023-03-13 19:40:41 +0000

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: {case_sensitive: false}, presence: true
+  validates :password, length: {minimum: 3}, presence: true
+
+  def authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.delete(' ').downcase)
+    if user && user.authenticate(password)
+      return user
+    end
+    nil
+  end
+
 end
--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -3,16 +3,35 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    @user = User.create({ first_name: "Giordano",
-      last_name: "Temple",
-      email: "gio@email.com",
-      password: "abc123",
-      password_confirmation: "abc123"
-      })
-
     it 'Saves the user' do
+      @user = User.new({ first_name: "Giordano",
+        last_name: "Temple",
+        email: "gio@email.com",
+        password: "abc123",
+        password_confirmation: "abc123"
+        })
       @user.save
       expect(User.find_by(id: 1)).not_to be_nil
+    end
+    it "Does not save the user when password's don't match" do
+      @user = User.new({ first_name: "Giordano",
+        last_name: "Tmofrpemple",
+        email: "gfmromfio@email.com",
+        password: "arfimros123",
+        password_confirmation: "different-password"
+        })
+      @user.save
+      expect(User.find_by(id: 2)).to be_nil
+    end
+    it "Does not save the user when password's don't match" do
+      @user = User.new({ first_name: "Giordano",
+        last_name: "moretest",
+        email: "testest@email.com",
+        password: "ab",
+        password_confirmation: "ab"
+        })
+      @user.save
+      expect(User.find_by(id: 2)).to be_nil
     end
   end
 end
